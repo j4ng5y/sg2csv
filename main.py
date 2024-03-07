b5084b63a2697dda715efa84ac2cb6403c1fd741
Jordan Gregory <jordan@j4ng5y.dev>
2024-03-07 08:26:57 -0600

    remove extra for loop, instance id to instance private dns name

--- main.py
+++ main.py
@@ -49,62 +49,61 @@ def main(args):
         )
         try:
             for instance in ec2.instances.all():
-                for sg in instance.security_groups:
-                    for sg in ec2.security_groups.all():
-                        for ip_permission in sg.ip_permissions:
-                            for ip_range in ip_permission["IpRanges"]:
-                                try:
-                                    fromPort = ip_permission["FromPort"]
-                                except KeyError:
-                                    fromPort = 0
-                                try:
-                                    toPort = ip_permission["ToPort"]
-                                except KeyError:
-                                    toPort = 0
-                                csv_writer.writerow(
-                                    [
-                                        "sgr",
-                                        f"{instance.id}",
-                                        f"{sg.group_id}",
-                                        f"{sg.description}",
-                                        "ingress",
-                                        f"{ip_permission['IpProtocol']}",
-                                        f"{fromPort}",
-                                        f"{toPort}",
-                                        f"{ip_range['CidrIp']}",
-                                    ]
-                                )
-                        for ip_permission_egress in sg.ip_permissions_egress:
-                            for ip_range in ip_permission_egress["IpRanges"]:
-                                try:
-                                    fromPort = ip_permission_egress["FromPort"]
-                                except KeyError:
-                                    fromPort = 0
-                                try:
-                                    toPort = ip_permission_egress["ToPort"]
-                                except KeyError:
-                                    toPort = 0
-                                csv_writer.writerow(
-                                    [
-                                        "sgr",
-                                        f"{instance.id}",
-                                        f"{sg.group_id}",
-                                        f"{sg.description}",
-                                        "egress",
-                                        f"{ip_permission_egress['IpProtocol']}",
-                                        f"{fromPort}",
-                                        f"{toPort}",
-                                        f"{ip_range['CidrIp']}",
-                                    ]
-                                )
-                        csv_writer.writerow(
-                            [
-                                "sg",
-                                f"{instance.private_dns_name}",
-                                f"{sg.group_id}",
-                                f"{sg.description}",
-                            ]
-                        )
+                for sg in ec2.security_groups.all():
+                    for ip_permission in sg.ip_permissions:
+                        for ip_range in ip_permission["IpRanges"]:
+                            try:
+                                fromPort = ip_permission["FromPort"]
+                            except KeyError:
+                                fromPort = 0
+                            try:
+                                toPort = ip_permission["ToPort"]
+                            except KeyError:
+                                toPort = 0
+                            csv_writer.writerow(
+                                [
+                                    "sgr",
+                                    f"{instance.private_dns_name}",
+                                    f"{sg.group_id}",
+                                    f"{sg.description}",
+                                    "ingress",
+                                    f"{ip_permission['IpProtocol']}",
+                                    f"{fromPort}",
+                                    f"{toPort}",
+                                    f"{ip_range['CidrIp']}",
+                                ]
+                            )
+                    for ip_permission_egress in sg.ip_permissions_egress:
+                        for ip_range in ip_permission_egress["IpRanges"]:
+                            try:
+                                fromPort = ip_permission_egress["FromPort"]
+                            except KeyError:
+                                fromPort = 0
+                            try:
+                                toPort = ip_permission_egress["ToPort"]
+                            except KeyError:
+                                toPort = 0
+                            csv_writer.writerow(
+                                [
+                                    "sgr",
+                                    f"{instance.private_dns_name}",
+                                    f"{sg.group_id}",
+                                    f"{sg.description}",
+                                    "egress",
+                                    f"{ip_permission_egress['IpProtocol']}",
+                                    f"{fromPort}",
+                                    f"{toPort}",
+                                    f"{ip_range['CidrIp']}",
+                                ]
+                            )
+                    csv_writer.writerow(
+                        [
+                            "sg",
+                            f"{instance.private_dns_name}",
+                            f"{sg.group_id}",
+                            f"{sg.description}",
+                        ]
+                    )
         except ClientError as e:
             print(e)
 
